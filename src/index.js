df9ccae47e7339f5d522e6de05e867cb9d59a3eb
James Cameron <jdonaldcameron@gmail.com>
2020-09-30 09:49:30 +1000

    feat: add custom mui theme support

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux';
+
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 // Initialize firebase
 import './firebase/fb';
 
 import store from './redux/store';
+import theme from './constants/theme';
 
 import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );

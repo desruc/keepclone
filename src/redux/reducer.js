da6565b237fd8bfde857511f71f666ef05c56f02
James Cameron <jdonaldcameron@gmail.com>
2020-09-30 07:30:40 +1000

    feat: init redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+import * as types from './types';
+
+const initialState = {
+  user: null
+};
+
+const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default appReducer;
